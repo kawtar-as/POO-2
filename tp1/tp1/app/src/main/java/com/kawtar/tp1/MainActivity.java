ac52fd9fa4bc85d3bf6838b6792dabdf333d8a03
Kawtar Ashil <keyvv7@gmail.com>
2026-03-12 01:00:36 -0400

    modification du tp

--- tp1/tp1/app/src/main/java/com/kawtar/tp1/MainActivity.java
+++ tp1/tp1/app/src/main/java/com/kawtar/tp1/MainActivity.java
@@ -2,6 +2,10 @@ package com.kawtar.tp1;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -17,6 +21,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     SurfaceDessin surface;
     Ecouteur ec;
@@ -24,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
     ConstraintLayout dessin;
 //    Button vert,rouge,rose,jaune,orange,blanc,noir,bleu;
     LinearLayout palette,outils;
+    Paint crayon,crayonActuel;
+    Path path;
+    Canvas canvas;
+    ArrayList<Path>paths;
+    ArrayList<Paint>crayons;
+    Point depart = new Point();
+    Point arrivee = new Point();
+    String color;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,24 +77,72 @@ public class MainActivity extends AppCompatActivity {
         // appuie sur la surface
         @Override
         public boolean onTouch(View v, MotionEvent event) {
-            return false;
+            // si on dessine libre
+
+
+            if( event.getAction() == event.ACTION_DOWN){
+                path= new Path();
+
+                //garder en mémoire le départ
+                depart.x = (int) event.getX();
+                depart.y = (int) event.getY();
+                path.moveTo(depart.x,depart.y);
+                // je cree un nv crayon
+                // pour que les couleurs des traits qu on a fait avant ne changent pas tous
+                Paint c = new Paint(crayon);
+                crayons.add(c);
+                paths.add(path);
+            }
+            else if(event.getAction() == event.ACTION_MOVE){
+                arrivee.x = (int) event.getX();
+                arrivee.y = (int) event.getY();
+                path.lineTo(arrivee.x, arrivee.y);
+                v.invalidate();
+
+            } else if (event.getAction() == event.ACTION_UP) {
+
+                depart.x = (int) event.getX();
+                depart.y = (int) event.getY();
+
+            }
+            return true;
         }
         @Override
         public void onClick(View v) {
+                System.out.println("allo");
+                // on get le tag ou on a mis la valeur de la couleur en hexa
+               color = v.getTag().toString();
+               // on converti en couleur de android
+               crayon.setColor(Color.parseColor(color)) ;
+
 
         }
 
     }
     private class SurfaceDessin extends View {
+
         public SurfaceDessin(Context context){
             super(context);
             // ici on change background/ les trucs de crayon
+            crayon = new Paint(Paint.ANTI_ALIAS_FLAG);
+            path = new Path();
+            paths = new ArrayList<>();
+            crayons = new ArrayList<>();
+            crayon.setColor(Color.BLACK);// par défaut
+            crayon.setStyle(Paint.Style.STROKE);
+            crayon.setStrokeWidth(15);
+
 
         }
 
         @Override
         protected void onDraw(@NonNull Canvas canvas) {
             super.onDraw(canvas);
+            for (int i = 0 ; i < paths.size() ; i++){
+                // on dessine chaque path dans la liste  avec un crayon choisi
+                canvas.drawPath(paths.get(i),crayons.get(i));
+            }
+            canvas.drawPath(path,crayon);
         }
     }
 
