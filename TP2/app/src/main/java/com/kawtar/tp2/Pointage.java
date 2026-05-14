9bb1267a96fbae0f6d1c02944a37eee800eb5ef0
kawtar Ashil <keyvv7@gmail.com>
2026-05-14 16:23:52 -0400

    debut de traail : ajout de l'interface , gestion bd et sous classe

---
+++ TP2/app/src/main/java/com/kawtar/tp2/Pointage.java
@@ -0,0 +1,20 @@
+package com.kawtar.tp2;
+
+public class Pointage {
+
+        private String date;
+        private int point;
+
+    public Pointage(String date, int point) {
+        this.date = date;
+        this.point = point;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+}
---
+++ TP2/app/src/main/java/com/kawtar/tp2/composante.java
@@ -0,0 +1,59 @@
+package com.kawtar.tp2;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+public class composante extends LinearLayout {
+    TextView multiplicateur,point,lettre;
+    public composante(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public composante(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public composante(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    public void init (Context context ){
+        LayoutInflater.from(context).inflate(R.layout.composante, this, true);
+        multiplicateur = findViewById(R.id.multiplicateur);
+        point = findViewById(R.id.point);
+        lettre = findViewById(R.id.lettre);
+
+    }
+
+    public TextView getMultiplicateur() {
+        return multiplicateur;
+    }
+
+    public void setMultiplicateur(TextView multiplicateur) {
+        this.multiplicateur = multiplicateur;
+    }
+
+    public TextView getPoint() {
+        return point;
+    }
+
+    public void setPoint(TextView point) {
+        this.point = point;
+    }
+
+    public TextView getLettre() {
+        return lettre;
+    }
+
+    public void setLettre(TextView lettre) {
+        this.lettre = lettre;
+    }
+}
