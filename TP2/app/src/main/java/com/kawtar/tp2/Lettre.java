d36a4460d1db79102c6240ef7ab9e3c197dd8111
Kawtar Ashil <keyvv7@gmail.com>
2026-05-16 23:00:24 -0400

    hwegdh

---
+++ TP2/app/src/main/java/com/kawtar/tp2/Lettre.java
@@ -0,0 +1,7 @@
+package com.kawtar.tp2;
+
+public class Lettre {
+    private char alphabet;
+    private int valeur;
+    private boolean compteDouble, compteTriple, motCompteDouble;
+}
---
+++ TP2/app/src/main/java/com/kawtar/tp2/Mot.java
@@ -0,0 +1,8 @@
+package com.kawtar.tp2;
+
+import java.util.ArrayList;
+
+public class Mot {
+    ArrayList<Lettre> lettres;
+    // methode calculer valeur qui fait la somme des valeur des lettres
+}
